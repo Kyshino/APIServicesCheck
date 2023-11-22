75f519aad6165278fee61bcd1be6eceeb8da7e42
Kyshino <delaplazaalberto@gmail.com>
2023-11-22 16:16:51 +0100

    check process

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan=require('morgan');
- 
+const exec = require('child_process').exec;
+
 //Configuraciones
 app.set('port', process.env.PORT || 3005);
 app.set('json spaces', 2)
@@ -10,14 +11,26 @@ app.set('json spaces', 2)
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+const isRunning = (query, cb) => {
+    let platform = process.platform;
+    let cmd = '';
+    switch (platform) {
+        case 'win32' : cmd = `tasklist`; break;
+        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
+        case 'linux' : cmd = `ps -A`; break;
+        default: break;
+    }
+    exec(cmd, (err, stdout, stderr) => {
+        cb(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
+    });
+}
  
 //Nuestro primer WS Get
-app.get('/honeygain', (req, res) => {    
-    res.json(
-        {
-            "Title": "Hola mundo"
-        }
-    );
+app.get('/earnapp', (req, res) => {    
+    isRunning('earnapp', (status) => {
+        return status;
+    });
 });
 
 app.get('/honeygain/claimer', (req, res) => {    
